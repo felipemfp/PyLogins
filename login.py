9995e2d7bfec64cbe6953b938f51f3109b04667c
CleytonRR <cleytonrodrigues69@live.com>
2016-12-20 21:36:22 -0300

    login.py

--- login.py
+++ login.py
@@ -1,51 +1,62 @@
 # coding: utf-8
 import time
 import base64
+import argparse
+import getpass
+
+def check(pasw):
+	sen = getpass.getpass("password again: ")
+	while pasw != sen:
+		print("Passwords do not match, try again")
+		sen = getpass.getpass("password: ")
+	return pasw
+
 def Hides(login):
 	arqui = open("logins.txt", "a")
 	codi = base64.b64encode(login.encode("utf-8", 'replace'))
 	codi = str(codi)
 	arqui.write(codi + "\n")
-	arqui.close
+	arqui.close()
+	print("Saved successfully")
 
-def save():
-	rede_social = input("Entre com o nome da rede social: ")
-	email = input("Entre com o seu email: ")
-	senha = input("entre com a sua senha: ")
-	login = ("Rede social: {rede} | Email: {email} | Senha: {senha}".format(rede = rede_social, email = email, senha = senha))
+def mold(usr, pasw):
+	login = ("Email: {e} | Senha: {s}".format(e = usr, s = pasw))
 	Hides(login)
 
 def show():
 	arqui = open("logins.txt", "r")
 	log = "logins"
 	while log != "":
-		log = arqui.readline()
-		log = str(log)
+		log = str(arqui.readline())
 		log = log[1:]
 		decodi = base64.b64decode(log)
 		print(decodi)
 	arqui.close()
 
-def menu(x):
-	if (x == 1):
-		save()
-	if (x == 2):
+def args():
+	log = argparse.ArgumentParser(description = 'Email e senha de um cadastro.')
+	log.add_argument("--e", action = "store", dest = "opc",
+		               required = False,
+		               help = "Enter argument and 'yes' to save logins")
+	log.add_argument("--s", action = "store", dest = "escolha",
+		               required = False,
+		               help = "Enter the argument and 'yes' to view logins")
+
+	data = log.parse_args()
+	if data.opc == "yes":
+		usr = input("Email: ")
+		pasw = getpass.getpass("password: ")
+		mold(usr, check(pasw))
+	elif data.escolha == "yes":
 		show()
 
 def main():
 	try:
-		with open("logins.txt", "r"):
-			print("Okay to start")
+		open("logins.txt", "r")
 	except IOError:
 		print("File not found\n*Creating file*")
 		time.sleep(5)
 		arqui = open("logins.txt", "w")
-	opc = 1
-	while(opc != 0):
-		opc = int(input("1- ADD Logins\n2- View logins\n0- exit\n"))
-		if opc == 0:
-			print("Thanks!")
-		else:
-			menu(opc)
+	args()
 if __name__ == '__main__':
 	main()
--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
 pt-br:
-Script feito em python para servir como um lembrete de seus logins. Você pode escrever seus logins durante a execução do script, o script vai gerar um arquivo criptografado com seus logins, os logins so poderam ser vistos com a execução do script.
+Script feito em python para servir como um lembrete de seus logins. Você pode escrever seus logins durante a execução do script, o script vai gerar um arquivo codificado em base64 com seus logins, os logins so poderam ser vistos com a execução do script.
+Para usá-lo você informa o parametro desejado acompanhado de "yes"
+ex: python3 login.py --e yes (Para salvar seus logins)
+ex2: python3 login.py --s yes (Para vizualizar logins salvos)
 
 en_US:
-Script made in python to serve as a reminder of your logins. You can write your logins while running the script, the script will generate a file encrypted with your logins, the logins can only be seen with the execution of the script
+Script made in python to serve as a reminder of your logins. You can write your logins during the execution of the script, the script will generate a file encoded in base64 with its logins, the logins can only be seen with the execution of the script.
+To use it you enter the desired parameter accompanied by "yes"
+ex: python3 login.py --e yes (To save your logins)
+ex2: python3 login.py --s yes (To view your logins)
